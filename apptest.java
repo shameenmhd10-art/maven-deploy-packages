c2f7a5f88bbe0b6dcc2480cff9501a4207800ae8
mhdshameen10 <shameenmhd10@gmail.com>
2026-07-07 16:35:24 +0530

    Add unit test for App class getMessage method

---
+++ apptest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testGetMessage() {
+        App app = new App();
+        assertEquals("Hello Jenkins CI/CD!", app.getMessage());
+    }
+}
